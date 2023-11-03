c2465d6c334c8b0dc0fa52570156182468feae4c
LebedevaAlina-zp <lebedeva.alina.zp@gmail.ru>
2023-11-03 15:05:10 +0800

    Parametized api get pets list tests. have some troubles with wrings logs for russian chinese and special chars.

--- tests/test_get_pets_list.py
+++ tests/test_get_pets_list.py
@@ -1,12 +1,15 @@
 import pytest
 from api import PetFriends
 from settings import valid_email, valid_password
+import generate_str
 
 pf = PetFriends()
 
 @pytest.mark.list
 @pytest.mark.positive
-def test_all_pets_valid_key(get_key, filter=''):
+@pytest.mark.parametrize('filter', ['', 'my_pets'],
+                         ids=['no filter', 'my_pets'])
+def test_valid_filter(get_key, filter):
     """Check the response status is 200 and the result contains a non-empty list of pets when a request via
     get_pets_list method with empty filter is made"""
 
@@ -15,26 +18,30 @@ def test_all_pets_valid_key(get_key, filter=''):
     assert 'pets' in result
     assert len(result['pets']) > 0
 
-@pytest.mark.list
-@pytest.mark.positive
-def test_my_pets(get_key, filter="my_pets"):
-    """Check that for a request for pets list with 'my_pets' filter the response has got non-empty list of pets.
-     Проверяем что запрос питомцев с фильтром "my_pets" возвращает непустой список питомцев."""
-
-    status, result = pf.get_pets_list(get_key, "my_pets")
-
-    assert status == 200
-    assert len(result['pets']) > 0
 
 @pytest.mark.list
 @pytest.mark.negative
-def test_all_pets_wrong_key(get_key, filter=''):
+@pytest.mark.parametrize('filter',
+                         [generate_str.n_string(255), generate_str.n_string(1000), generate_str.russian_chars(),
+                          generate_str.chinese_chars(), generate_str.special_chars()],
+                         ids=['255 chars', '1000 chars', 'russian chars', 'chinese chars', 'special chars'])
+def test_unacceptable_filter(get_key, filter):
+    """Check the response code is 400 for all unacceptable filter values sent with a request for pets list."""
+    status, result = pf.get_pets_list(get_key, filter)
+    assert status == 400
+
+@pytest.mark.list
+@pytest.mark.negative
+@pytest.mark.parametrize('auth_key',
+                         [{'key': ''}, {'key': f'{generate_str.n_string(255)}'},
+                          {'key': f'{generate_str.n_string(1000)}'}, {'key': f'{generate_str.russian_chars()}'},
+                          {'key': f'{generate_str.chinese_chars()}'}, {'key': f'{generate_str.special_chars()}'},
+                          {'key': 5}],
+                         ids=['empty str', '255 chars', '1000 chars', 'russian chars', 'chinese chars', 'special chars',
+                              'integer'])
+def test_my_pets_wrong_key(auth_key, filter='my_pets'):
     """Check a user can't get a list of pets with a wrong auth_key."""
 
-    # Delete the last symbol in the key obtained
-    wrong_key = get_key.copy()
-    wrong_key['key'] = wrong_key['key'][:-1]
-
-    status, result = pf.get_pets_list(wrong_key, filter)
+    status, result = pf.get_pets_list(auth_key, filter)
 
     assert status == 403
